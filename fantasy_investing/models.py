abda9efb4bec23a4c00a06d0e2d5112d566166ec
jhtae <jhtae92@gmail.com>
2017-04-03 16:50:07 -0700

    Add portolio model

--- fantasy_investing/models.py
+++ fantasy_investing/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Portfolio(models.Model):
+	title = models.CharField=(250);
+	user = models.ForeignKey(User, on_delete=model.CASCADE)
