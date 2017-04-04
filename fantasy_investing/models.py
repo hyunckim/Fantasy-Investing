f6aaddfe0560488ae7bf91c2221f98f00e954ce7
jhtae <jhtae92@gmail.com>
2017-04-03 19:18:28 -0700

    Fix portfolio model

--- fantasy_investing/models.py
+++ fantasy_investing/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Portfolio(models.Model):
-	title = models.CharField=(250);
-	user = models.ForeignKey(User, on_delete=model.CASCADE)
+	title = models.CharField(max_length=250);
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
     purchase_price = models.FloatField(blank=False, default=0)
     purchase_date = models.DateField(auto_now=True)
     number_of_shares = models.IntegerField(blank=False, default=0)
-    # portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
