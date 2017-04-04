30b1671691333b7138ed2be666675b2c9ba913b6
hyunckim <hyunkim1209@gmail.com>
2017-04-03 23:49:26 -0700

    Add Serializer model for Company

--- fantasy_investing/serializers.py
+++ fantasy_investing/serializers.py
@@ -10,3 +10,8 @@ class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ('ticker', 'purchase_price', 'purchase_date', 'number_of_shares', 'portfolio')
+
+class CompanySerializer(serializers.Serializer):
+    ticker = serializers.CharField(max_length=10)
+    title = serializers.CharField(max_length=100)
+    price = serializers.FloatField(default=0)
--- fantasy_investing/views.py
+++ fantasy_investing/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from yahoo_finance import Share
 
-# Create your views here.
+@csrf_exempt
+
+def stock_detail(request, pk):
+    try:
+        stock = Stock.objects.get(pk=pk)
+    except Stock.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        
