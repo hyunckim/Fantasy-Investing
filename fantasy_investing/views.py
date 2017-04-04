8ea178f7a86df8417a39750b5a46cdc95152655f
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-04-03 19:12:37 -0700

    Initial commit

--- fantasy_investing/views.py
+++ fantasy_investing/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
-# Create your views here.
+class UserFormView(View):
+    # form_class = UserForm
+    # template_name = 'fantasy_investing/login_form.html'
+
+    def get(self, request):
+        username = request.GET['username']
+        password = request.GET
+
+
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        user = {}
+        user['username'] = request.POST['username']
+        user['password'] = request.POST['password']
+        user['email'] = request.POST['email']
+        user['balance'] = request.POST['balance']
+
+        if user.save()
+            return render()
