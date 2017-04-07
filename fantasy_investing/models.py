63e104b014850b3c57ab14136ef395745a246138
hyunckim <hyunkim1209@gmail.com>
2017-04-06 19:56:01 -0700

    resolve migration issues

--- fantasy_investing/models.py
+++ fantasy_investing/models.py
@@ -5,8 +5,8 @@ from django.db.models.signals import post_save
 class Portfolio(models.Model):
     title = models.CharField(max_length=250)
     main = models.BooleanField(null=False, default=False)
-	user = models.ForeignKey(User, default=1)
-
+    user = models.ForeignKey(User, default=1)
+	
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
     purchase_price = models.FloatField(default=0)
