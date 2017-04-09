835901bacaea035a1009ed551e798fc6804d244c
jhtae <jhtae92@gmail.com>
2017-04-08 21:20:40 -0700

    Merge conflict

--- frontend/components/portfolio/portfolio_container.js
+++ frontend/components/portfolio/portfolio_container.js
@@ -3,7 +3,6 @@ import { fetchPortfolios } from '../../actions/portfolio_actions';
 import Portfolio from './portfolio';
 
 const mapStateToProps = state => {
-<<<<<<< HEAD
     return {
         currentUser: state.currentUser,
         portfolio: state.portfolio
@@ -15,17 +14,6 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         fetchPortfolios: () => dispatch(fetchPortfolios())
     });
 };
-=======
-  return ({
-    currentUser: state.currentUser,
-    portfolio: Object.keys(state.portfolio).map(id => state.portfolio[id])
-  });
-};
-
-const mapDispatchToProps = (dispatch, ownProps) => ({
-    fetchPortfolios: () => dispatch(fetchPortfolios())
-});
->>>>>>> master
 
 export default connect(
   mapStateToProps,
