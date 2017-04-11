8a17e896eeb50a57a4c4d45c81d886ef7427789f
hyunckim <hyunkim1209@gmail.com>
2017-04-10 23:27:15 -0700

    update database setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fantasyinvesting',
         'USER': '',
-        'PASSWORD': ''
+        'PASSWORD': '',
         # 'HOST': 'localhost',
         'PORT': '5432',
     }
