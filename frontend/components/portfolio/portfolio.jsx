b481ab2ec793e51de31f7d933b28937b6c8426bf
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-04-26 17:08:18 -0700

    Add index fetch

--- frontend/components/portfolio/portfolio.jsx
+++ frontend/components/portfolio/portfolio.jsx
@@ -38,7 +38,6 @@ class Portfolio extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-      debugger;
       if (nextProps.portfolio.length === this.props.portfolio.length) {
         for (let i = 0; i < nextProps.portfolio.length; i++) {
           if (nextProps.portfolio[i].stocks.length > this.props.portfolio[i].stocks.length) {
@@ -70,6 +69,8 @@ class Portfolio extends React.Component {
       }
       mainTickers = mainTickers.slice(0,-1);
       watchlistTickers = watchlistTickers.slice(0,-1);
+      let indexTickers = '$SPX,$DJI,$RUT';
+      let etfTickers = "SPY,DIA,IWM";
       let priceData = [];
 
       if (mainTickers.length > 0) {
@@ -78,21 +79,26 @@ class Portfolio extends React.Component {
       if (watchlistTickers.length > 0) {
         this.fetchData(watchlistTickers, 'name,last_price,change,adj_high_price,adj_low_price,52_week_high,52_week_low,adj_volume,average_daily_volume,marketcap,industry_group');
       }
+      this.fetchData(indexTickers, 'close_price');
+      this.fetchData(etfTickers, 'percent_change');
     }
 
     fetchData(tickers, items, index = 0) {
       let username = [
-        "d6166222f6cd23d2214f20c0de1d4cc3",
-        "0f51c94416c5a029ced069c9c445bcf4",
-        "77a9accfe589ee1bde92b347cd7243bf",
-        "00c96699cb9905e2e93939af22fd255d",
-        "9543da974ae42ceb2724f4fc215bb83b",
-        "1b4f66213e0ee9c96e1298adaf093d99",
-        "4d28e4bb9ba48a3e05e0f7d5e03fe130",
-        "db165ed10432182a47f5439432be10b6",
-        "9bbbdbda7c369c21969cdc108fef9a87",
-        "ef2c9c791fd32dcb138fc9ca511a651c",
-        ];
+       "d6166222f6cd23d2214f20c0de1d4cc3",
+       "0f51c94416c5a029ced069c9c445bcf4",
+       "77a9accfe589ee1bde92b347cd7243bf",
+       "00c96699cb9905e2e93939af22fd255d",
+       "9543da974ae42ceb2724f4fc215bb83b",
+       "1b4f66213e0ee9c96e1298adaf093d99",
+       "4d28e4bb9ba48a3e05e0f7d5e03fe130",
+       "db165ed10432182a47f5439432be10b6",
+       "9bbbdbda7c369c21969cdc108fef9a87",
+       "ef2c9c791fd32dcb138fc9ca511a651c",
+       "6b3f930579a063f2593bee515e0ce231",
+       "3ee4fd6b79113d9a021f8edc344cde15",
+       "cd25157222f897581b38dfa05a0dc94b"
+     ];
       let password = [
         "6fbb48d898d18930d6fc1e2d4e1bd54b",
         "dfb23653432156bdbf868393255d9f3d",
@@ -103,7 +109,10 @@ class Portfolio extends React.Component {
         "286ce4fbedd72511eac4dd3e58831c67",
         "5a59201505bf41ef2e52f5c15e123fd7",
         "2fa44779f963571608242cfc9d216cd2",
-        "4a9214f9a7031f8870897deb8cbdd488"
+        "4a9214f9a7031f8870897deb8cbdd488",
+        "4c14c89a57db9522f6c8f460e3142d88",
+        "a5792311c4b1288b385afdb57b8378a8",
+        "fe24c4e4e4196c7ddd1fd7bfb0bd8f8e"
         ];
       $.ajax({
           type: "GET",
@@ -123,31 +132,36 @@ class Portfolio extends React.Component {
     }
 
     receiveNews(ticker, index = 0) {
-<<<<<<< HEAD
-    let username = [
-      "d6166222f6cd23d2214f20c0de1d4cc3",
-      "0f51c94416c5a029ced069c9c445bcf4",
-      "77a9accfe589ee1bde92b347cd7243bf",
-      "00c96699cb9905e2e93939af22fd255d",
-      "9543da974ae42ceb2724f4fc215bb83b",
-      "1b4f66213e0ee9c96e1298adaf093d99",
-      "4d28e4bb9ba48a3e05e0f7d5e03fe130",
-      "db165ed10432182a47f5439432be10b6",
-      "9bbbdbda7c369c21969cdc108fef9a87",
-      "ef2c9c791fd32dcb138fc9ca511a651c",
-      ];
-    let password = [
-      "6fbb48d898d18930d6fc1e2d4e1bd54b",
-      "dfb23653432156bdbf868393255d9f3d",
-      "6fabe9c15bd1e7ead66b7cc3cd6b3e44",
-      "2ce4b7bb869b8c78e176ee210c20269d",
-      "1f91849f806fe320b31c550ebe39bae9",
-      "2e11b74611f8e7a5f52f68a8e04c88b7",
-      "286ce4fbedd72511eac4dd3e58831c67",
-      "5a59201505bf41ef2e52f5c15e123fd7",
-      "2fa44779f963571608242cfc9d216cd2",
-      "4a9214f9a7031f8870897deb8cbdd488"
-      ];
+      let username = [
+       "d6166222f6cd23d2214f20c0de1d4cc3",
+       "0f51c94416c5a029ced069c9c445bcf4",
+       "77a9accfe589ee1bde92b347cd7243bf",
+       "00c96699cb9905e2e93939af22fd255d",
+       "9543da974ae42ceb2724f4fc215bb83b",
+       "1b4f66213e0ee9c96e1298adaf093d99",
+       "4d28e4bb9ba48a3e05e0f7d5e03fe130",
+       "db165ed10432182a47f5439432be10b6",
+       "9bbbdbda7c369c21969cdc108fef9a87",
+       "ef2c9c791fd32dcb138fc9ca511a651c",
+       "6b3f930579a063f2593bee515e0ce231",
+       "3ee4fd6b79113d9a021f8edc344cde15",
+       "cd25157222f897581b38dfa05a0dc94b"
+     ];
+      let password = [
+        "6fbb48d898d18930d6fc1e2d4e1bd54b",
+        "dfb23653432156bdbf868393255d9f3d",
+        "6fabe9c15bd1e7ead66b7cc3cd6b3e44",
+        "2ce4b7bb869b8c78e176ee210c20269d",
+        "1f91849f806fe320b31c550ebe39bae9",
+        "2e11b74611f8e7a5f52f68a8e04c88b7",
+        "286ce4fbedd72511eac4dd3e58831c67",
+        "5a59201505bf41ef2e52f5c15e123fd7",
+        "2fa44779f963571608242cfc9d216cd2",
+        "4a9214f9a7031f8870897deb8cbdd488",
+        "4c14c89a57db9522f6c8f460e3142d88",
+        "a5792311c4b1288b385afdb57b8378a8",
+        "fe24c4e4e4196c7ddd1fd7bfb0bd8f8e"
+        ];
     $.ajax({
       type: "GET",
       url: `https://api.intrinio.com/news?ticker=${ticker}&page_size=20`,
@@ -395,6 +409,34 @@ class Portfolio extends React.Component {
         //   }
 
         //   }
+        let indexHtml = (<div></div>);
+        if (this.data['$SPX']) {
+          let spyPrev = this.numberWithCommas(Math.round(this.data["$SPX"]['close_price'] * 100) / 100);
+          let djiPrev = this.numberWithCommas(Math.round(this.data["$DJI"]['close_price'] * 100) / 100);
+          let nyaPrev = this.numberWithCommas(Math.round(this.data["$NYA"]['close_price'] * 100) / 100);
+          let spyPercent = this.data["SPY"]['percent_change'] * 100;
+          let djiPercent = this.data["DIA"]['percent_change'] * 100;
+          let nyaPercent = this.data["IWM"]['percent_change'] * 100;
+          let spyPercent = (this.data["$SPX"]['change'] /
+            (this.data["$SPX"]['close_price'] + this.data["$SPX"]['change'])
+            * 100).toFixed(2);
+
+          indexHtml = (
+            <div className="indices-container">
+              <div>
+                S&P 500
+                <p>{spyLast}</p>
+                <p>{spyChange} {spyPercent}</p>
+              </div>
+              <div>
+                Dow 30
+                <p>{djiLast}</p>
+                <p>{djiChange}</p>
+              </div>
+            </div>
+          );
+        }
+
         let portfolioTable;
         let portfolioIndex = [];
         let mainPortfolio = this.state.currentPortfolio;
@@ -526,7 +568,6 @@ class Portfolio extends React.Component {
         let value = (<p></p>);
         let className = 'portfolio-green';
         if (mainPortfolio && mainPortfolio.main) {
-          debugger;
           if (percentageChange < 0) {
             className = 'portfolio-red';
           }
@@ -537,7 +578,7 @@ class Portfolio extends React.Component {
                 <p className={className} id="portfolio-change"  >  <span>${this.numberWithCommas(Math.round(unrealizedGain))}</span>   ({percentageChange.toFixed(1)}%)</p>
               </div>
               );
-          }else if((percentageChange || percentageChange === 0) && className === 'portfolio-green') {
+          } else if((percentageChange || percentageChange === 0) && className === 'portfolio-green') {
             value = (
               <div className='portfolio-performance'>
                 <p className='total-value'>${this.numberWithCommas(Math.round(totalValue))}</p>
@@ -678,6 +719,10 @@ class Portfolio extends React.Component {
                     <div className='current-portfolio-title'>
                       <span>{mainPortfolio.title}</span>
                     </div>
+                    <div>
+                      { indexHtml }
+                    </div>
+
                   </div>
                   <div className='portfolio-mid'>
                     <div className='mid-header'>
